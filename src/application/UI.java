dd1c779ac779e1ca97d040bf5c8588c745fe4665
Maxuel Junior <56198827+maxueljunior@users.noreply.github.com>
2022-06-30 19:34:57 -0300

    Resolvido problema de aparecer null

--- src/application/UI.java
+++ src/application/UI.java
@@ -9,14 +9,11 @@ public class UI {
 		for(int i=0; i<pieces.length; i++) {
 			System.out.print((8-i) + " ");
 			for(int j=0;j<pieces.length; j++) {
-				System.out.print(pieces[i][j]);
+				printPiece(pieces[i][j]);
 			}
 			System.out.println();
 		}
 		System.out.println("  a b c d e f g h");
-	
-		
-		
 	}
 	
 	private static void printPiece(ChessPiece piece) {
